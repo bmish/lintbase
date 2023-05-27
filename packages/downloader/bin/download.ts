24afd9f0f6f6115ff0400899a73e4e73e0204106
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2023-05-27 12:24:03 -0400

    switch to npm install individual packages before loading them

--- packages/downloader/bin/download.ts
+++ packages/downloader/bin/download.ts
@@ -1,8 +1,14 @@
-import pacote from 'pacote';
 import fetch from 'node-fetch';
 import fs from 'node:fs';
 import path from 'node:path';
 import url from 'node:url';
+import { createRequire } from 'node:module';
+import { exec } from 'node:child_process';
+import util from 'node:util';
+
+const execP = util.promisify(exec);
+
+const require = createRequire(import.meta.url);
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
@@ -10,34 +16,32 @@ type NpmSearchResult = {
   objects: { package: { name: string; version: string } }[];
 };
 
-async function downloadPackage(
-  packageName: string,
-  version: string,
-  downloadPath: string
-): Promise<void> {
-  const packageDir = path.join(downloadPath, packageName, version);
-
-  // Create a directory for the package if it doesn't exist
-  if (fs.existsSync(packageDir)) {
-    console.log(`Already downloaded: ${packageName}@${version}`);
-  } else {
-    fs.mkdirSync(packageDir, { recursive: true });
-
-    await pacote.extract(`${packageName}@${version}`, packageDir);
-    console.log(`Package downloaded successfully: ${packageName}@${version}`);
-  }
-}
-
 async function downloadPackages(
   searchText: string,
   downloadPath: string
-): Promise<void> {
+): Promise<{ name: string; version: string }[]> {
   const data = await downloadJSON<NpmSearchResult>(
     `https://registry.npmjs.org/-/v1/search?text=${searchText}&size=1000`
   );
-  for (const pkg of data.objects) {
-    await downloadPackage(pkg.package.name, pkg.package.version, downloadPath);
+
+  const packagesAndVersions = data.objects.map((pkg) => ({
+    name: pkg.package.name,
+    version: pkg.package.version, // Unused.
+  }));
+
+  // TODO: try to batch install for performance.
+  for (const pkg of packagesAndVersions.slice(0, 10)) {
+    console.log(`Installing ${pkg.name}@${pkg.version}`);
+    const command = `npm install ${pkg.name} --prefix ${downloadPath} -f`;
+
+    try {
+      await execP(command);
+    } catch (error) {
+      console.error(`Failed to install ${pkg.name}. Error: ${String(error)}`);
+    }
   }
+
+  return packagesAndVersions;
 }
 
 async function downloadJSON<T>(url: string): Promise<T> {
@@ -56,4 +60,27 @@ async function downloadJSON<T>(url: string): Promise<T> {
 const keyword = 'eslint-plugin';
 const downloadPath = path.join(__dirname, '..', 'tmp', 'npm-packages');
 
-await downloadPackages(keyword, downloadPath);
+const packages = await downloadPackages(keyword, downloadPath);
+
+console.log(
+  packages.flatMap((pkg) => {
+    // ignore if this package doesn't have package.json
+    const packagePath = path.join(downloadPath, 'node_modules', pkg.name);
+    if (!fs.existsSync(packagePath)) {
+      console.log(`No package.json found for ${pkg.name}@${pkg.version}`);
+
+      return [];
+    }
+
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return, import/no-dynamic-require
+      return require(packagePath);
+    } catch (error) {
+      console.log(
+        `Failed to require: ${packagePath}. Error = ${String(error)}`
+      );
+    }
+
+    return [];
+  })
+);
