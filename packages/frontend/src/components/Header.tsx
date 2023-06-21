2a46964351304db7d938f9e789fe0e5b0e41ee37
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2023-06-21 10:56:14 -0400

    more responsive page navigation, remove unimlemented hamburger menu

--- packages/frontend/src/components/Header.tsx
+++ packages/frontend/src/components/Header.tsx
@@ -41,59 +41,44 @@ export default function Header() {
 
   return (
     <header className="sticky top-0 z-30 w-full px-2 py-4 bg-white sm:px-4 shadow-xl ">
-      <div className="flex items-center justify-between mx-auto max-w-7xl">
+      <div className="flex items-center justify-between mx-auto max-w-7xl space-x-2">
         <Link href="/">
           <Image src={'/logo.png'} width={200} height={55} alt="Logo" />
         </Link>
 
-        <div className="flex items-center space-x-1">
-          <ul className="hidden space-x-2 md:inline-flex">
-            <li>
-              <TextField
-                type="search"
-                placeholder="Search"
-                variant="standard"
-                value={searchValue}
-                onChange={handleChange} // TODO: add debouncing
-              ></TextField>
-            </li>
-            <li>
-              <Link
-                href="/db"
-                className="px-4 py-2 font-semibold text-gray-600 rounded"
-              >
-                Database
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/about"
-                className="px-4 py-2 font-semibold text-gray-600 rounded"
-              >
-                About
-              </Link>
-            </li>
-          </ul>
-          <div className="inline-flex md:hidden">
-            <button className="flex-none px-2 ">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 8h16M4 16h16"
-                />
-              </svg>
-              <span className="sr-only">Open Menu</span>
-            </button>
-          </div>
-        </div>
+        <ul className="space-x-0 sm:space-x-2 inline-flex">
+          <li>
+            <TextField
+              type="search"
+              placeholder="Search"
+              variant="standard"
+              value={searchValue}
+              onChange={handleChange} // TODO: add debouncing
+            ></TextField>
+          </li>
+          <li>
+            <Link
+              href="/db"
+              className="px-2 sm:px-4 py-2 font-semibold text-gray-600 rounded hidden sm:inline"
+            >
+              Database
+            </Link>
+            <Link
+              href="/db"
+              className="px-2 sm:px-4 py-2 font-semibold text-gray-600 rounded sm:hidden"
+            >
+              DB
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/about"
+              className="px-2 sm:px-4 py-2 font-semibold text-gray-600 rounded"
+            >
+              About
+            </Link>
+          </li>
+        </ul>
       </div>
     </header>
   );
