08f493eb883ef5074040c571a05e04f80744ddf8
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2023-07-19 12:00:35 -0400

    remove google forms link for announcements, rely on waitlist instead

--- packages/frontend/src/pages/about/index.tsx
+++ packages/frontend/src/pages/about/index.tsx
@@ -18,34 +18,19 @@ export default function About() {
       </Head>
       <main className="py-8 px-6 max-w-4xl mx-auto min-h-screen">
         <Grid container spacing={4}>
-          <Grid item xs={12} md={6}>
+          <Grid item xs={12} md={12}>
             <Card>
               <CardContent>
                 <Typography variant="h6" marginBottom={2}>
                   Message us
                 </Typography>
                 <p>
-                  Bug reports, suggestions for additional linters to support,
-                  etc...
+                  Feedback, bug reports, suggestions for additional linters to
+                  support, etc...
                 </p>
               </CardContent>
               <CardActions>
                 <Button href="mailto:info@lintbase.com">Email</Button>
-              </CardActions>
-            </Card>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Card>
-              <CardContent>
-                <Typography variant="h6" marginBottom={2}>
-                  Sign up for announcements
-                </Typography>
-                <p>Hear about our latest developments...</p>
-              </CardContent>
-              <CardActions>
-                <Button href="https://docs.google.com/forms/d/e/1FAIpQLSfc5yLA4DVIYsNAQVc-I-0By0fizM1gxJ96YjP23oVHg7Ku5A/viewform">
-                  Sign up
-                </Button>
               </CardActions>
             </Card>
           </Grid>
