fc103b74a88133eccb72bbe64a40e4efdca32523
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2023-08-02 17:58:20 +0800

    move creation of packages/linters for repo to refresh instead of import

--- packages/frontend/src/server/api/routers/repository.ts
+++ packages/frontend/src/server/api/routers/repository.ts
@@ -16,32 +16,6 @@ export const repositoryRouter = createTRPCRouter({
       })
     )
     .mutation(async ({ ctx, input }) => {
-      const octokit = new Octokit({
-        auth: env.GITHUB_PERSONAL_ACCESS_TOKEN,
-      });
-
-      // TODO: search recursively for local packages in monorepos.
-      const contents = (await octokit.request(
-        'GET /repos/{owner}/{repo}/contents',
-        {
-          owner: input.fullName.split('/')[0],
-          repo: input.fullName.split('/')[1],
-          headers: {
-            'X-GitHub-Api-Version': '2022-11-28',
-          },
-        }
-      )) as { data: { name: string; path: string }[] };
-
-      // TODO: support other lint frameworks.
-      const lintFramework = await ctx.prisma.lintFramework.findFirstOrThrow({
-        where: {
-          name: 'eslint',
-          ecosystem: {
-            name: 'node',
-          },
-        },
-      });
-
       await ctx.prisma.repository.create({
         data: {
           fullName: input.fullName,
@@ -51,24 +25,6 @@ export const repositoryRouter = createTRPCRouter({
           size: input.size,
           importedAt: new Date(),
           owner: { connect: { id: ctx.session.user.id } },
-          localPackages: {
-            create: contents.data
-              .filter((data) => data.name === 'package.json')
-              .map((data) => ({
-                path: data.path,
-                localPackageLintFrameworks: {
-                  create: contents.data
-                    .filter((data) => data.name === '.eslintrc.js')
-                    .map((data) => ({
-                      pathConfig: data.path,
-                      isPresent: true,
-                      lintFramework: {
-                        connect: { id: lintFramework.id },
-                      },
-                    })),
-                },
-              })),
-          },
         },
       });
     }),
@@ -96,12 +52,77 @@ export const repositoryRouter = createTRPCRouter({
       );
       const lastCommit = commits.data[0];
 
+      // TODO: search recursively for local packages in monorepos.
+      const contents = (await octokit.request(
+        'GET /repos/{owner}/{repo}/contents',
+        {
+          owner: input.fullName.split('/')[0],
+          repo: input.fullName.split('/')[1],
+          headers: {
+            'X-GitHub-Api-Version': '2022-11-28',
+          },
+        }
+      )) as { data: { name: string; path: string }[] };
+
+      // TODO: support other lint frameworks.
+      const lintFramework = await ctx.prisma.lintFramework.findFirstOrThrow({
+        where: {
+          name: 'eslint',
+          ecosystem: {
+            name: 'node',
+          },
+        },
+      });
+
+      // TODO: eventually reuse existing models that are still relevant.
+      const deleteLocalPackages = ctx.prisma.localPackage.deleteMany({
+        where: {
+          repository: {
+            owner: { id: ctx.session.user.id },
+            fullName: input.fullName,
+          },
+        },
+      });
+      const deleteLocalPackageLintFrameworks =
+        ctx.prisma.localPackageLintFramework.deleteMany({
+          where: {
+            localPackage: {
+              repository: {
+                owner: { id: ctx.session.user.id },
+                fullName: input.fullName,
+              },
+            },
+          },
+        });
+      await ctx.prisma.$transaction([
+        deleteLocalPackageLintFrameworks,
+        deleteLocalPackages,
+      ]); // Must be done in a transaction to satisfy constraints.
+
       await ctx.prisma.repository.update({
         where: { owner: { id: ctx.session.user.id }, fullName: input.fullName },
         data: {
           commitSha: lastCommit.sha,
           committedAt: lastCommit.commit.committer?.date,
           scannedAt: new Date(),
+          localPackages: {
+            create: contents.data
+              .filter((data) => data.name === 'package.json')
+              .map((data) => ({
+                path: data.path,
+                localPackageLintFrameworks: {
+                  create: contents.data
+                    .filter((data) => data.name === '.eslintrc.js')
+                    .map((data) => ({
+                      pathConfig: data.path,
+                      isPresent: true,
+                      lintFramework: {
+                        connect: { id: lintFramework.id },
+                      },
+                    })),
+                },
+              })),
+          },
           // TODO: update anything else
         },
       });
