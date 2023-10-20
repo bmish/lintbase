787619a12c3828322a00a7927a5315dde1b7541b
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2023-10-20 16:15:32 -0400

    tweak PrismaClient code in case of future client extension

--- packages/frontend/src/server/db.ts
+++ packages/frontend/src/server/db.ts
@@ -1,16 +1,18 @@
 import { PrismaClient } from '@prisma/client';
 import { env } from '@/env.mjs';
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
+function getClient() {
+  return new PrismaClient({
     log:
       env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   });
+}
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof getClient> | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? getClient();
 
 if (env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
