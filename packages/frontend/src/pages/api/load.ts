fec96236dce9bfb8cdf60b7165e678cade636dbb
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2023-10-25 19:20:18 -0400

    fix load commented out section

--- packages/frontend/src/pages/api/load.ts
+++ packages/frontend/src/pages/api/load.ts
@@ -9,15 +9,15 @@ export default async function load(req: NextApiRequest, res: NextApiResponse) {
   }
 
   // TODO: temporary fix to prevent "module not found" error during Vercel deployment due to this import that only works locally.
-  // const eslintRules = {
-  //   entries() {
-  //     return [];
-  //   },
-  // };
-  const { default: eslintRules } = await import(
-    // @ts-expect-error -- ESLint doesn't have types.
-    '../../../../downloader/tmp/npm/eslint/node_modules/eslint/lib/rules/index.js'
-  );
+  const eslintRules = {
+    entries() {
+      return [];
+    },
+  };
+  // const { default: eslintRules } = await import(
+  //   // @ts-expect-error -- ESLint doesn't have types.
+  //   '../../../../downloader/tmp/npm/eslint/node_modules/eslint/lib/rules/index.js'
+  // );
 
   const lintersCreated = await loadLintersToDb(
     Object.fromEntries(eslintRules.entries()) // Convert from LazyLoadingRuleMap to standard object.
