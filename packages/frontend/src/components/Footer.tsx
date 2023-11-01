0a5763e33d0b9eea976b5390a41f0c364f3f7c56
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2023-10-31 22:37:21 -0400

    footer: automatic year for copyright notice

--- packages/frontend/src/components/Footer.tsx
+++ packages/frontend/src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <section>
       <div className="container mx-auto py-10 max-w-3xl text-center">
-        © 2023 Lintbase
+        © {new Date().getFullYear()} Lintbase
       </div>
     </section>
   );
